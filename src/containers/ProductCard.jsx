8c474beaf78c4a54b684da8054ca7585d096ee6e
jmedina <ndgns2@gmail.com>
2023-04-03 16:11:17 -0600

    Export repeatitive code

---
+++ src/containers/ProductCard.jsx
@@ -0,0 +1,29 @@
+import { IconButton, ImageListItem, ImageListItemBar } from "@mui/material";
+import InfoIcon from "@mui/icons-material/Info";
+import React from "react";
+import { Link } from "react-router-dom";
+
+const ProductCard = ({ product }) => {
+  const { _id, url, title, size } = product;
+  return (
+    <ImageListItem className="image-list-container">
+      <img src={url} srcSet={url} alt={title} loading="lazy" />
+      <ImageListItemBar
+        title={title}
+        subtitle={`Size: ${size}`}
+        actionIcon={
+          <Link to={`/product/${_id}`}>
+            <IconButton
+              sx={{ color: "rgba(255, 255, 255, 0.54)" }}
+              aria-label={`info about ${title}`}
+            >
+              <InfoIcon />
+            </IconButton>
+          </Link>
+        }
+      />
+    </ImageListItem>
+  );
+};
+
+export default ProductCard;
